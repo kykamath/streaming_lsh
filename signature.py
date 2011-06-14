62a0b22ddd917bde702735207312c10d80c4eb79
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-14 16:30:02 -0500

    added signature

---
+++ signature.py
@@ -0,0 +1,9 @@
+'''
+Created on Jun 14, 2011
+
+@author: kykamath
+'''
+from bitarray import bitarray
+
+class Signature(bitarray):
+    pass
--- temp.py
+++ temp.py
@@ -3,5 +3,21 @@ Created on Jun 13, 2011
 
 @author: kykamath
 '''
-from library.file_io import FileIO
-FileIO.createDirectoryForFile('/home/kykamath/temp/asdfsd/abc')
+
+from signature import Signature
+from library.vector import Vector, VectorGenerator
+
+def getSignatureForVector(vector, unitRandomVectors): return Signature(vector.dot(rv)>=0 for rv in unitRandomVectors)
+
+
+numberOfDocuments = 5
+#dimension = 10
+dimension=2
+numberOfRandomVectors = 13
+
+documents = [Vector({1:1,2:2}), Vector({1:1,2:1}), Vector({1:2,2:1}) ,Vector({1:10,2:2}), Vector({1:-5,2:-10})]
+#documents = [VectorGenerator.getRandomGaussianUnitVector(dimension, 0, 1) for i in range(numberOfDocuments)]
+unitRandomVectors = [VectorGenerator.getRandomGaussianUnitVector(dimension, 0, 1) for i in range(numberOfRandomVectors)]
+for doc in documents: 
+    print doc
+    print getSignatureForVector(doc, unitRandomVectors)
