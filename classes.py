a9ae8de5a7658c3c10deb4d39db457ea4774602e
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-15 16:13:41 -0500

    modified classes.

--- classes.py
+++ classes.py
@@ -3,26 +3,67 @@ Created on Jun 14, 2011
 
 @author: kykamath
 '''
-from bitarray import bitarray
 import random
+from bitarray import bitarray
+from Bio import trie
+
+class SignatureTrie:
+    @staticmethod
+    def getNearestSignatureKey(trie, signature):
+        digitReplacement = {'0': '1', '1': '0'}
+        targetKey, iteratingKey = signature.to01(), ''
+        for i in range(len(targetKey)):
+            iteratingKey+=targetKey[i]
+            if not trie.has_prefix(iteratingKey): iteratingKey=iteratingKey[:-1]+digitReplacement[targetKey[i]]
+        return iteratingKey
 
 class Permutation:
     def __init__(self, signatureLength): 
         self.p, self.a, self.b = signatureLength, random.choice(range(signatureLength)[1::2]), random.choice(range(signatureLength))
-        self.documentSignatures = {}
+        self.signatureTrie = trie.trie()
     def apply(self, x): return (self.a*x+self.b)%self.p
+    def addDocument(self, document):
+        signatureKey = document.signature.permutate(self).to01()
+        if self.signatureTrie.has_key(signatureKey): self.signatureTrie[signatureKey].add(document.docId)
+        else: self.signatureTrie[signatureKey] = set([document.docId])
+    def getNearestDocuments(self, document):
+        nearestSignatureKey=SignatureTrie.getNearestSignatureKey(self.signatureTrie, document.signature)
+        return self.signatureTrie[nearestSignatureKey]
+        
     def __str__(self): return 'p: %s, a: %s, b: %s'%(self.p, self.a, self.b)
     
 class Signature(bitarray):
     def permutate(self, permutation): return Signature([self[permutation.apply(x)] for x in xrange(len(self))])
-    @staticmethod
-    def sorted(signatures): 
-        print sorted(signatures)
-        
+    
 class Document:
     def __init__(self, docId, vector): self.docId, self.vector = docId, vector
     def initializeSignatureForVector(self, unitRandomVectors): self.signature = Signature(self.vector.dot(rv)>=0 for rv in unitRandomVectors)
     def __str__(self): return str(self.vector)
     
 if __name__ == '__main__':
-    print dict((i, 'a') for i in xrange(4))
+#    def match_all(string, trie):
+#        matches = []
+#        for i in range(len(string)):
+#            substr = string[:i+1]
+#            if not trie.has_prefix(substr):
+#                break
+#            if trie.has_key(substr):
+#                matches.append(substr)
+#        return matches
+#    
+#    
+#    from Bio import trie, triefind
+#    from bitarray import bitarray
+#    trieObject= trie.trie()
+#    trieObject['1101']=23
+#    trieObject['1100']=12
+#    trieObject[bitarray('101').to01()]=15
+
+    a = 'asd'
+    a=a[:-1]+'a'
+    print a
+
+    
+#    print match_all('1101', trieObject)
+    
+        
--- tests/classes_tests.py
+++ tests/classes_tests.py
@@ -4,36 +4,49 @@ Created on Jun 14, 2011
 @author: kykamath
 '''
 import unittest
-from classes import Signature, Permutation
+from Bio import trie
+from classes import Signature, Permutation, SignatureTrie
 
 class SignatureTests(unittest.TestCase):
-    
     def test_initialization(self):
         sgnt = Signature('1001011')
         self.assertEqual(sgnt.count(), 4)
         sgnt = Signature()
         self.assertEqual(sgnt.count(), 0)
-        
     def test_permutate(self):
         sgnt = Signature('1001011')
         self.assertTrue(sgnt.count()==sgnt.permutate(Permutation(7)).count())
         
-#    def test_sorted(self):
-#        signatures = [Signature('10'), Signature('11'), Signature('00'), Signature('01')]
-#        Signature.sorted(signatures)
+class SignatureTrieTests(unittest.TestCase):
+    def setUp(self):
+        self.tr = trie.trie()
+        self.tr['1000']=12;self.tr['1011']=12; self.tr['1010']=12
+        
+    def test_getNearestSignature_exactKey(self): self.assertEquals(SignatureTrie.getNearestSignatureKey(self.tr, Signature('1000')), '1000')
+    
+    def test_getNearestSignature_nearbyKey(self): self.assertEquals(SignatureTrie.getNearestSignatureKey(self.tr, Signature('1100')), '1000')    
         
 class PermutationTests(unittest.TestCase):
-    
     def setUp(self):
         self.pm = Permutation(13)
-    
     def test_initialization(self):
         self.assertTrue(self.pm.a<self.pm.p and self.pm.b<self.pm.p)
         self.assertTrue(self.pm.a%2!=0)
-    
     def test_permutationFunction(self):
         l = [self.pm.apply(i) for i in range(self.pm.p)]
         self.assertEqual(sorted(l), range(self.pm.p))
+    def test_addDocument_newKey(self):
+        pass
+    def test_addDocument_existingKey(self):
+        pass
+    def test_removeDocument(self):
+        pass
+    def test_getNearestDocument_existingKey(self):
+        pass
+    def test_getNearestDocument_nearestKey(self):
+        pass
+    def test_getNearestDocument_multipleDocumentsReturned(self):
+        pass
         
 if __name__ == '__main__':
     unittest.main()
