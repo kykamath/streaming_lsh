c5d5f4a78f6fdfb32015ca74e668eda841f3c666
kykamath <krishna.kamath@gmail.com>
2011-06-18 11:55:36 -0500

    modified classes

--- demo/OnlineLSHDemo.py
+++ demo/OnlineLSHDemo.py
@@ -5,12 +5,34 @@ Created on Jun 16, 2011
 '''
 
 from library.file_io import FileIO 
+from classes import Document, RandomGaussianUnitVector,\
+    RandomGaussianUnitVectorPermutation, SignaturePermutation
+from library.vector import Vector
 
 class OnlineLSHDemo:
     @staticmethod
     def demo():
-        for line in FileIO.iterateLinesFromFile('../data/streaming.dat'):
-            print line
+        wordToDimensionMap = {}
+        def createDocumentFromLine(docId, line):
+            vector = Vector()
+            words = line.split()
+            for word in words[1:]:
+                if word not in wordToDimensionMap: wordToDimensionMap[word]=len(wordToDimensionMap)
+                wordDimension = wordToDimensionMap[word]
+                if wordDimension not in vector: vector[wordDimension]=1
+                else: vector[wordDimension]+=1
+            return Document(docId, vector, clusterType=words[0])
+        
+        dimensions = 53
+        signatureLength=13
+        numberOfPermutations = 5
+        
+        randomGaussianUnitVector = RandomGaussianUnitVector(dimensions=dimensions, mu=0, sigma=1)
+        randomGaussianUnitVectorPermutations = RandomGaussianUnitVectorPermutation.getPermutations(signatureLength, dimensions, randomGaussianUnitVector)
+        signaturePermutations = [SignaturePermutation(signatureLength) for i in range(numberOfPermutations)]
+        
+        
+#        for line in FileIO.iterateLinesFromFile('../data/streaming.dat'):
 
 if __name__ == '__main__':
     OnlineLSHDemo().demo()
