eef2d2eeaa832c6af5f7abc50d42c86b075eb6a6
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-18 21:37:57 -0500

    added gcd

--- math_modified.py
+++ math_modified.py
@@ -18,3 +18,14 @@ def isPrime(number):
     The original author can be contacted at nolfonzo@gmail.com
     '''
     return number in filter(lambda num: (num % numpy.arange(2,1+int(math.sqrt(num)))).all(), range(2,number+1))
+
+def gcd(numA, numB):
+    '''
+    The method to determine greatest common divisor by Andrew Pociu
+    http://www.geekpedia.com/code120_Find-The-Greatest-Common-Divisor.html
+    '''
+    while numB != 0:
+        numRem = numA % numB
+        numA = numB
+        numB = numRem
+    return numA
--- tests/math_modified_tests.py
+++ tests/math_modified_tests.py
@@ -5,7 +5,7 @@ Created on Jun 17, 2011
 '''
 
 import unittest
-from math_modified import isPrime
+from math_modified import isPrime, gcd
 
 class MathModifiedTests(unittest.TestCase):
     def test_isPrime(self):
@@ -15,6 +15,8 @@ class MathModifiedTests(unittest.TestCase):
         self.assertTrue(isPrime(13))
         self.assertFalse(isPrime(10))
         self.assertTrue(isPrime(101))
+    
+    def test_gcd(self): self.assertEqual(3, gcd(30,21))
         
 if __name__ == '__main__':
     unittest.main()
