88e564b951b0bba21583072b8ab147c7ed51155b
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-22 13:53:42 -0500

    modified readme

--- nlp.py
+++ nlp.py
@@ -4,7 +4,10 @@ Created on Jun 14, 2011
 @author: kykamath
 '''
 
-import enchant
+import enchant, cjson
+
+twitter_stop_words_file='data/stop_words.json'
+twitter_stop_words_over_threshold_percentage = 0.5
 
 enchantDict = enchant.Dict("en_US")
 def isEnglish(sentance, threshold=0.3):
@@ -15,3 +18,19 @@ def isEnglish(sentance, threshold=0.3):
     except Exception: pass
     if englishWords/totalWords > threshold: return True
     return False
+
+class StopWords:
+    list = None
+    @staticmethod
+    def load(extra_terms=['#p2', '#ff', '#fb']):
+        if StopWords.list == None: 
+            StopWords.list = {}
+            stop_word_candidates = cjson.load(open(twitter_stop_words_file))
+            for stop_word_candidate in stop_word_candidates:
+                if stop_word_candidates[stop_word_candidate]['ot'] >= twitter_stop_words_over_threshold_percentage: StopWords.list[stop_word_candidate]=True
+            for term in extra_terms: StopWords.list[term] = True
+    @staticmethod
+    def contains(word):
+        try:
+            return StopWords.list[word]
+        except KeyError: return False
