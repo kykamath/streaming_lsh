3c417fd45752499a80759645c91c08c2a8825af4
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-23 23:42:45 -0500

    modified nlp

--- classes.py
+++ classes.py
@@ -4,8 +4,14 @@ Created on Jun 22, 2011
 @author: kykamath
 '''
 class Settings(dict):
-    def __getattr__(self, name):
-        return self[name]
-
-    def __setattr__(self, name, value):
-        self[name] = value
+    def __getattr__(self, name): return self[name]
+    def __setattr__(self, name, value): self[name] = value
+        
+class GeneralMethods:
+    callMethodEveryIntervalVariable=None
+    @staticmethod
+    def callMethodEveryInterval(method, interval, currentTime, **kwargs):
+        if GeneralMethods.callMethodEveryIntervalVariable==None: GeneralMethods.callMethodEveryIntervalVariable=currentTime
+        if currentTime-GeneralMethods.callMethodEveryIntervalVariable>=interval:
+            method(**kwargs)
+            GeneralMethods.callMethodEveryIntervalVariable=currentTime
