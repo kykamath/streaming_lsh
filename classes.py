16d12f7e69108548d6c3f0f8801a4abe72f3262d
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-24 18:49:39 -0500

    modified classes

--- classes.py
+++ classes.py
@@ -4,6 +4,10 @@ Created on Jun 22, 2011
 @author: kykamath
 '''
 class Settings(dict):
+    '''
+    This class was obtained from Jeff McGee.
+    https://github.com/jeffamcgee
+    '''
     def __getattr__(self, name): return self[name]
     def __setattr__(self, name, value): self[name] = value
         
