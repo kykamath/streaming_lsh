2aa09eafa75696e0b6651551d6854bccfac638dd
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-25 15:33:03 -0500

    modified streaming

--- StreamingLSHClustering.py
+++ StreamingLSHClustering.py
@@ -7,7 +7,7 @@ from classes import RandomGaussianUnitVector, VectorPermutation,\
     SignaturePermutation, Cluster
 from operator import itemgetter
 
-class StreamingLSHClustering:
+class StreamingLSHClustering(object):
     def __init__(self, **clustering_settings):
         self.thresholdForDocumentToBeInACluster = clustering_settings['threshold_for_document_to_be_in_cluster']
         self.unitVector = RandomGaussianUnitVector(dimensions=clustering_settings['dimensions'], mu=0, sigma=1)
