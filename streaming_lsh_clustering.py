2140e9b5b022ae87724d0ed8622d0c38dc5f58ae
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-27 18:51:38 -0500

    minor modifications

--- streaming_lsh_clustering.py
+++ streaming_lsh_clustering.py
@@ -9,8 +9,6 @@ from operator import itemgetter
 from library.classes import TwoWayMap
 
 class StreamingLSHClustering(object):
-    PHRASE_TEXT_TO_DIMENSION = TwoWayMap.MAP_FORWARD
-    DIMENSION_TO_PHRASE_TEXT = TwoWayMap.MAP_REVERSE
     def __init__(self, **clustering_settings):
         self.thresholdForDocumentToBeInACluster = clustering_settings['threshold_for_document_to_be_in_cluster']
         self.unitVector = RandomGaussianUnitVector(dimensions=clustering_settings['dimensions'], mu=0, sigma=1)
