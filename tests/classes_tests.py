4d37e2078bc98d10150b2e89d02671399dc19ec1
Krishna Kamath <krishna.kamath@gmail.com>
2011-06-29 00:12:41 -0500

    minor changes

--- tests/classes_tests.py
+++ tests/classes_tests.py
@@ -19,13 +19,17 @@ class GeneralMethodsTests(unittest.TestCase):
         while currentTime<=final_time:
             GeneralMethods.callMethodEveryInterval(method, timedelta(minutes=15), currentTime, arg1=15, arg2=currentTime)
             currentTime+=timedelta(minutes=1)
-
+    def test_reverseDict(self):
+        self.assertEqual({1:'a', 2:'b'}, GeneralMethods.reverseDict({'a':1, 'b':2}))
+        self.assertRaises(Exception, GeneralMethods.reverseDict, {'a':1, 'b':1})
+        
 class TwoWayDictTests(unittest.TestCase):
     def setUp(self):
         self.twoWayMap = TwoWayMap()
         self.assertRaises(TypeError, self.twoWayMap.set, (5, 1, 2))
         self.twoWayMap.set(TwoWayMap.MAP_FORWARD, 'a', 'A')
         self.twoWayMap.set(TwoWayMap.MAP_REVERSE, 'B', 'b')
+    def __isValidObject(self): return self.twoWayMap.getMap(TwoWayMap.MAP_REVERSE)==GeneralMethods.reverseDict(self.twoWayMap.getMap(TwoWayMap.MAP_FORWARD))
     def test_basicOperation(self):
         self.assertEqual('A', self.twoWayMap.get(TwoWayMap.MAP_FORWARD, 'a'))
         self.assertEqual('a', self.twoWayMap.get(TwoWayMap.MAP_REVERSE, 'A'))
@@ -33,10 +37,17 @@ class TwoWayDictTests(unittest.TestCase):
         self.assertEqual('b', self.twoWayMap.get(TwoWayMap.MAP_REVERSE, 'B'))
         self.assertEqual({'b': 'B', 'a': 'A'}, self.twoWayMap.getMap(self.twoWayMap.MAP_FORWARD))
         self.assertEqual({'B': 'b', 'A': 'a'}, self.twoWayMap.getMap(self.twoWayMap.MAP_REVERSE))
+        self.assertTrue(self.__isValidObject())
+    def test_set(self):
+        self.twoWayMap.set(TwoWayMap.MAP_FORWARD, 'c', 'A'), self.assertTrue(self.__isValidObject())
+        self.twoWayMap.set(TwoWayMap.MAP_REVERSE, 'C', 'b'), self.assertTrue(self.__isValidObject())
+        self.twoWayMap.set(TwoWayMap.MAP_FORWARD, 'a', 'D'), self.assertTrue(self.__isValidObject())
+        self.twoWayMap.set(TwoWayMap.MAP_REVERSE, 'B', 'd'), self.assertTrue(self.__isValidObject())
     def test_delete(self):
         self.twoWayMap.remove(TwoWayMap.MAP_FORWARD, 'a')
         self.assertEqual({'b': 'B'}, self.twoWayMap.getMap(self.twoWayMap.MAP_FORWARD))
         self.assertEqual({'B': 'b'}, self.twoWayMap.getMap(self.twoWayMap.MAP_REVERSE))
+        self.assertTrue(self.__isValidObject())
     def test_length(self):
         self.assertEqual(2, len(self.twoWayMap))
         self.twoWayMap.set(TwoWayMap.MAP_REVERSE, 'C', 'c')
@@ -45,5 +56,6 @@ class TwoWayDictTests(unittest.TestCase):
         self.assertTrue(self.twoWayMap.contains(TwoWayMap.MAP_FORWARD, 'a'))
         self.assertFalse(self.twoWayMap.contains(TwoWayMap.MAP_FORWARD, 'A'))
         self.assertTrue(self.twoWayMap.contains(TwoWayMap.MAP_REVERSE, 'B'))
+        
 if __name__ == '__main__':
     unittest.main()
