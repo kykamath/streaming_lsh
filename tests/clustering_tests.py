5e5a9957714d349a8a8eaed041be7bcc14139568
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-04 17:40:43 -0500

    Added clustering code

--- tests/clustering_tests.py
+++ tests/clustering_tests.py
@@ -4,9 +4,10 @@ Created on Jun 21, 2011
 @author: kykamath
 '''
 import sys
+from classes import TwoWayMap
 sys.path.append('../')
 import unittest
-from clustering import EvaluationMetrics
+from clustering import EvaluationMetrics, EMTextClustering
 
 class EvaluationMetricsTests(unittest.TestCase):
     def setUp(self):
@@ -18,6 +19,14 @@ class EvaluationMetricsTests(unittest.TestCase):
     def test_getValueForClusters(self): self.assertEqual(0.875, EvaluationMetrics.getValueForClusters(self.clusters, EvaluationMetrics.purity))
     def test_getValueForClustersWithEmpltyClusters(self): self.assertEqual(0.0, EvaluationMetrics.getValueForClusters([[]], EvaluationMetrics.purity))
     
-    
+class EMTextClusteringTests(unittest.TestCase):
+    def test_convertDocumentsToVector(self):
+        documents = [
+                     (1, 'a b c d f g'),
+                     (2, 'a b c d t h'),
+                     (3, '1 2 3 4 5'),
+                     (4, '1 2 3 ')
+                     ]
+        print EMTextClustering(documents,2).cluster()
 if __name__ == '__main__':
     unittest.main()
