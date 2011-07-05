7851ae4c00df36a2d30f955ff6baf0c121c437f7
Krishna Kamath <krishna.kamath@gmail.com>
2011-07-05 16:03:34 -0500

    added plotting module

--- plotting.py
+++ plotting.py
@@ -10,6 +10,11 @@ import scipy.optimize
 def getLatexForString(str): return '$'+str.replace(' ', '\\ ')+'$'
 
 class CurveFit():
+    '''
+    Exponential funcion: a.x^-b
+    '''
+    exponentialFunction = lambda p, x: p[0]*pow(x, -1*p[1])
+    
     def __init__(self, functionToFit, initialParameters, dataX, dataY): 
         self.functionToFit, self.initialParameters, self.dataX, self.dataY = functionToFit, initialParameters, dataX, dataY
         if self.functionToFit != None: self.error = lambda p, x, y: self.functionToFit(p, x) - y
@@ -25,3 +30,17 @@ class CurveFit():
         plt.xlabel(xlabel), plt.ylabel(ylabel), plt.title(title)
         plt.show()
     def getModeledYValues(self): return self.functionToFit(self.actualParameters, self.dataX)
+    @staticmethod
+    def getParamsAfterFittingData(x, y, functionToFit, initialParameters):
+        cf = CurveFit(functionToFit, initialParameters, x, y)
+        cf.estimate()
+        return cf.actualParameters
+    @staticmethod
+    def iterator(functionToFit, params, x): 
+        for i in x: yield functionToFit(params, i)
+    @staticmethod
+    def getParamsForExponentialFitting(x,y): return CurveFit.getParamsAfterFittingData(x, y, CurveFit.exponentialFunction, [1., 1.])
+    @staticmethod
+    def exponentialIterator(params,x): CurveFit.iterator(CurveFit.exponentialFunction, params, x)
+        
+    
