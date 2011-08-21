8d2d7f05a9b087ad703333992c6430d70cb7e3b7
Krishna Kamath <krishna.kamath@gmail.com>
2011-08-21 12:23:57 -0500

    added signature lists

--- streaming_lsh/streaming_lsh_clustering.py
+++ streaming_lsh/streaming_lsh_clustering.py
@@ -4,7 +4,7 @@ Created on Jun 25, 2011
 @author: kykamath
 '''
 from classes import RandomGaussianUnitVector, VectorPermutation,\
-    SignaturePermutation, Cluster, UtilityMethods
+    SignaturePermutationWithTrie, Cluster, UtilityMethods
 from operator import itemgetter
 from library.classes import TwoWayMap
 
@@ -13,7 +13,7 @@ class StreamingLSHClustering(object):
         self.thresholdForDocumentToBeInACluster = clustering_settings['threshold_for_document_to_be_in_cluster']
         self.unitVector = RandomGaussianUnitVector(dimensions=clustering_settings['dimensions'], mu=0, sigma=1)
         self.vectorPermutations = VectorPermutation.getPermutations(clustering_settings['signature_length'], clustering_settings['dimensions'], self.unitVector)
-        self.signaturePermutations = [SignaturePermutation(clustering_settings['signature_length']) for i in range(clustering_settings['number_of_permutations'])]
+        self.signaturePermutations = [SignaturePermutationWithTrie(clustering_settings['signature_length']) for i in range(clustering_settings['number_of_permutations'])]
         self.phraseTextAndDimensionMap, self.clusters = TwoWayMap(), {}
         self.clustering_settings = clustering_settings
     
