2746834b1ddb18d04728de3cf939d44f71a0b3b6
Krishna Kamath <krishna.kamath@gmail.com>
2011-09-16 19:06:19 -0500

    modified nns

--- streaming_lsh/nearest_neighbor_lsh.py
+++ streaming_lsh/nearest_neighbor_lsh.py
@@ -4,7 +4,8 @@ Created on Sep 16, 2011
 @author: kykamath
 '''
 from classes import RandomGaussianUnitVector, VectorPermutation,\
-    SignaturePermutationWithTrie, UtilityMethods, Document
+    SignaturePermutationWithTrie, UtilityMethods, Document,\
+    SignaturePermutationWithSortedList
 from operator import itemgetter
 from library.classes import TwoWayMap
 
@@ -14,7 +15,12 @@ class NearestNeighborUsingLSH(object):
         self.nearestNeighborThreshold = settings['nearest_neighbor_threshold']
         self.unitVector = RandomGaussianUnitVector(dimensions=settings['dimensions'], mu=0, sigma=1)
         self.vectorPermutations = VectorPermutation.getPermutations(settings['signature_length'], settings['dimensions'], self.unitVector)
-        self.signaturePermutations = [SignaturePermutationWithTrie(settings['signature_length']) for i in range(settings['number_of_permutations'])]
+#        self.signaturePermutations = [SignaturePermutationWithTrie(settings['signature_length']) for i in range(settings['number_of_permutations'])]
+        
+        signatureType = settings.get('signature_type', 'signature_type_trie')
+        if signatureType=='signature_type_trie': self.signaturePermutations = [SignaturePermutationWithTrie(settings['signature_length']) for i in range(settings['number_of_permutations'])]
+        else: self.signaturePermutations = [SignaturePermutationWithSortedList(settings['signature_length']) for i in range(settings['number_of_permutations'])]
+        
         self.phraseTextAndDimensionMap = TwoWayMap()
         self.documentIdToDocumentMap = {}
     
@@ -26,11 +32,31 @@ class NearestNeighborUsingLSH(object):
         for permutation in self.signaturePermutations: 
             if currentDocument!=None: permutation.removeDocument(currentDocument)
             permutation.addDocument(newDocument)
+            
+    def _removeDocument(self, document):
+        documentToRemove = self.documentIdToDocumentMap.get(document.docId, None)
+        if documentToRemove!=None:
+            for permutation in self.signaturePermutations: permutation.removeDocument(documentToRemove)
+        del self.documentIdToDocumentMap[document.docId]
         
+    def _addDocument(self, document):
+        self.documentIdToDocumentMap[document.docId] = document
+        document.setSignatureUsingVectorPermutations(self.unitVector, self.vectorPermutations, self.phraseTextAndDimensionMap)
+        for permutation in self.signaturePermutations: permutation.addDocument(document)
+    
     def getNearestDocument(self, document):
         UtilityMethods.updatePhraseTextAndDimensionsMap(document, self.phraseTextAndDimensionMap, **self.settings)
         document.setSignatureUsingVectorPermutations(self.unitVector, self.vectorPermutations, self.phraseTextAndDimensionMap)
         predictedNeighbor = None
         possibleNearestNeighbors = reduce(lambda x,y:x.union(y), (permutation.getNearestDocuments(document) for permutation in self.signaturePermutations), set())
         if possibleNearestNeighbors: predictedNeighbor = max(((docId, document.cosineSimilarity(document)) for docId in possibleNearestNeighbors), key=itemgetter(1))
-        if predictedNeighbor and predictedNeighbor[1]>=self.nearestNeighborThreshold:return predictedNeighbor[0]    
+        if predictedNeighbor and predictedNeighbor[1]>=self.nearestNeighborThreshold:return predictedNeighbor[0]
+    
+    def getNearestDocumentWithReplacement(self, document):
+#        document.setSignatureUsingVectorPermutations(self.unitVector, self.vectorPermutations, self.phraseTextAndDimensionMap)
+#        print document.signature
+        print self.getNearestDocument(document)
+        self._removeDocument(document)
+        documentToReturn = self.getNearestDocument(document)
+        self._addDocument(document)
+        return documentToReturn
--- streaming_lsh/tests/nearest_neighbor_lsh_tests.py
+++ streaming_lsh/tests/nearest_neighbor_lsh_tests.py
@@ -13,6 +13,7 @@ from classes import Document
 nns_settings = {'dimensions': 53,
                 'signature_length': 13,
                 'number_of_permutations': 5,
+                'signature_type': 'signature_type_lists',
                 'nearest_neighbor_threshold': 0.5}
 
 def createDocumentFromLine(docId, line):
@@ -21,21 +22,24 @@ def createDocumentFromLine(docId, line):
         if word not in vector: vector[word]=1
         else: vector[word]+=1
     return Document(words[0], vector)
+i = 0
+documents = []
+for line in FileIO.iterateLinesFromFile('../data/streaming.dat'):
+    documents.append(createDocumentFromLine(None, line)); i+=1
+    if i==10: break
 
 class NearestNeighborUsingLSHTests(unittest.TestCase):
     def setUp(self):
         self.nnsLSH = NearestNeighborUsingLSH(**nns_settings)
-        self.documents = []
-        i = 0
-        for line in FileIO.iterateLinesFromFile('../data/streaming.dat'):
-            self.documents.append(createDocumentFromLine(None, line)); i+=1
-            if i==10: break
-            
     def test_nns(self):
-        for d in self.documents: 
+        for d in documents: 
             self.nnsLSH.update(d)
             self.assertEqual(d.docId, self.nnsLSH.getNearestDocument(d))
+    def test_getNearestDocumentWithReplacement(self):
+        for d in documents: self.nnsLSH.update(d)
+        for d in documents: print d.docId, self.nnsLSH.getNearestDocument(d)
         
+    
     
 if __name__ == '__main__':
     unittest.main()
